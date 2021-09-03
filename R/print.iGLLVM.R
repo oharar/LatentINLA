0c921d958ed689e1c28aac05dbe333369827a996
BertvdV <bert_van_der_veen@hotmail.com>
2021-09-03 11:22:51 +0200

    Did a woopsie

--- R/print.iGLLVM.R
+++ R/print.iGLLVM.R
@@ -4,7 +4,7 @@ print.iGLLVM <- function(x, ...) {
   cat("Call: \n")
   print(x$call)
   cat("\n")
-  cat("family:",paste(names(mod$family)," (",as.numeric(mod$family),")",sep=""))
+  cat("family:",paste(names(x$family)," (",as.numeric(x$family),")",sep=""))
   cat("\n\n")
   cat("marginal log-likelihood: ", x$LL, "\n")
 }

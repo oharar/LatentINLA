d5fbeb0e595cc69ea4a3d79f633b217733f75036
bertv <bert_van_der_veen@hotmail.com>
2023-02-06 22:32:13 +0100

    fix test

--- tests/testthat/test-CreateFormulaRHS.R
+++ tests/testthat/test-CreateFormulaRHS.R
@@ -16,7 +16,7 @@ test_that("CreateFormulaRHS works correctly", {
   expect_equal(nchar(Formula), 731)
   expect_equal(nchar(FormulaB), 724)
   expect_equal(grepl("param=c(0, 0.25)", FormulaB, fixed=TRUE), TRUE)
-  expect_equal(nchar(FormulaC), 851)
+  expect_equal(nchar(FormulaC), 858)
 
   expect_equal(length(FormulaD), 1) # If this fails, we're in trouble
   expect_equal(nchar(FormulaD), 793)

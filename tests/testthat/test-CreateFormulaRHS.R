56e6f7de00bee0a3a148052375dd074163a577ed
Bob O'Hara <robertoh@dhcp-10-24-17-117.wlan.ntnu.no>
2023-02-20 13:57:31 +0100

    Update CreateFormulaRHS tests, as CreateFormulaRHS() now creates a formula object

--- tests/testthat/test-CreateFormulaRHS.R
+++ tests/testthat/test-CreateFormulaRHS.R
@@ -12,14 +12,14 @@ test_that("CreateFormulaRHS works correctly", {
   FormulaD <- CreateFormulaRHS(LVs, constrained=FALSE, hyper = "list(oops)")
 
   # Test size
-  expect_equal(length(Formula), 1) # If this fails, we're in trouble
-  expect_equal(nchar(Formula), 731)
-  expect_equal(nchar(FormulaB), 724)
-  expect_equal(grepl("param=c(0, 0.25)", FormulaB, fixed=TRUE), TRUE)
-  expect_equal(nchar(FormulaC), 858)
+  expect_equal(length(Formula), 3) # If this fails, we're in trouble
+  expect_equal(nchar(Formula), c(1,1,814))
+  expect_equal(nchar(FormulaB), c(1,1,807))
+  expect_equal(grepl("param = c(0, 0.25)", FormulaB, fixed=TRUE)[3], TRUE)
+  expect_equal(nchar(FormulaC), c(1,1,917))
 
-  expect_equal(length(FormulaD), 1) # If this fails, we're in trouble
-  expect_equal(nchar(FormulaD), 793)
-  expect_equal(grepl("oops", FormulaD, fixed=TRUE), TRUE)
+  expect_equal(length(FormulaD), 3) # If this fails, we're in trouble
+  expect_equal(nchar(FormulaD), c(1,1,880))
+  expect_equal(grepl("oops", FormulaD, fixed=TRUE)[3], TRUE)
 
 })
